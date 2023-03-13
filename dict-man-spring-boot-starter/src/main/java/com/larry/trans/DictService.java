b4860659702dcf7248e630dbf7c76e829a8dae4f
lingen <1762861715@qq.com>
2023-03-13 17:38:52 +0800

    修正

--- dict-man-spring-boot-starter/src/main/java/com/larry/trans/DictService.java
+++ dict-man-spring-boot-starter/src/main/java/com/larry/trans/DictService.java
@@ -7,33 +7,28 @@ import java.util.Map;
 
 
 public class DictService {
-    private static final ThreadLocal<Map<String, Map<String, String>>> dictType = new ThreadLocal<>();
+    private final Map<String, Map<String, String>> dictType = new HashMap<>();
 
     // 比如字典是id转换名称,不需要加类别,因为雪花id之类生成策略已经保证分布式下也唯一。
     public void putDictItem(String key, String value) {
-        Map<String, String> dictMan = getDict("dictMan");
-        if (dictMan != null && dictMan.size() > 0) {
-            dictMan.put(key, value);
-            putDictType("dictMan",dictMan );
-
-        }else{
-            putDictType("dictMan",new HashMap() {{
-                put(key, value);
-            }} );
+        Map<String, String> dictMan = dictType.get("dictMan");
+        if(dictMan==null){
+            dictMan = new HashMap<>();
         }
-
+        dictMan. put(key, value);
+        putDictType("dictMan", dictMan);
     }
 
     // 分类
     public void putDictType(String key, Map<String, String> item) {
-        Map<String, Map<String, String>> stringMapMap = dictType.get();
-        stringMapMap.put(key, item);
-        dictType.set(stringMapMap);
+        dictType.put(key, item);
 
     }
 
     public Map<String, String> getDict(String key) {
-        Map<String, Map<String, String>> stringMapMap = dictType.get();
-        return stringMapMap.get(key);
+        Map<String, String> map = dictType.get(key);
+        if( map == null) map = new HashMap<>();
+        return map;
     }
 }
+
